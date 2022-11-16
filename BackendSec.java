fccfe4752ed3e2a6ae3957f009f912efb1dd6817
Ashish N M <76056451+Ashish93-mrx@users.noreply.github.com>
2022-11-16 13:27:58 +0530

    Create BackendSec.java

---
+++ BackendSec.java
@@ -0,0 +1,17 @@
+package com.cambotutorial.sovary.myapplication;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class NoficationActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nofication);
+        setContentView(R.layout.activity_nofication);
+    }
+}
