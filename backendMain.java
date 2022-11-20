745535c18197366e6da3f15dd93c6a668e4f7c48
Ashish N M <76056451+Ashish93-mrx@users.noreply.github.com>
2022-11-20 12:35:28 +0530

    Update backendMain.java
    Added Option location and modified some code

--- backendMain.java
+++ backendMain.java
@@ -31,7 +31,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Intent;
+import android.content.Intent; 
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
